d6e26beed1b61937c6f0ead664096967ecafa9ba
Diego Martinez <55361667+dmartinezm@users.noreply.github.com>
2019-11-21 17:22:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,4 +13,99 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
 
+  fetchToys()
+  createNewToy()
+
 })
+
+let fetchToys= () => {
+  const toysURL = 'http://localhost:3000/toys'
+ fetch(toysURL)
+ .then(res => res.json())
+ .then(buildList)
+
+}
+
+let buildList = (allToys) => {
+  allToys.forEach(addDivToy)
+}
+
+ let addDivToy = (toyObject) => {
+  //  console.log(toyObject)
+   let toyCollectionDiv = document.querySelector('#toy-collection')
+   let toyDiv = document.createElement('div')
+   toyDiv.className = 'card'
+   let toyH2 = document.createElement('h2')
+   toyH2.innerText = toyObject.name
+   let toyImg = document.createElement('img')
+    toyImg.src = toyObject.image
+    toyImg.className = 'toy-avatar'
+   let toyP = document.createElement('p')
+   toyP.innerText = toyObject.likes
+   let toyButtonLikes = document.createElement('button')
+   toyButtonLikes.className = 'like-btn'
+   toyButtonLikes.innerText = 'Like'
+   
+   toyButtonLikes.addEventListener("click", () => {
+    toyObject.likes++ 
+    likeToy(toyObject,toyP)
+   })
+   toyDiv.append(toyH2,toyImg,toyP,toyButtonLikes)
+   toyCollectionDiv.append(toyDiv)
+
+ }
+
+ let likeToy = (toyObject, toyP) => {
+  let indToyUrl = `http://localhost:3000/toys/${toyObject.id}`
+  fetch(indToyUrl, {
+    method:'PATCH',
+   headers: { 
+       'Content-type': 'application/json',
+       'accept': 'application/json'
+   },
+   body: JSON.stringify({
+   likes: toyObject.likes
+    })
+  })
+  .then(resp => resp.json())
+  .then(json_resp => {
+    toyP.innerText = toyObject.likes
+
+  })
+ }
+
+ let createNewToy = () => {
+  const newToyForm = document.querySelector('.container')
+  newToyForm.addEventListener('submit',(event) => {
+    event.preventDefault()
+
+    const toysURL = 'http://localhost:3000/toys'
+    let toyName = event.target.name.value
+    let toyImage = event.target.image.value
+    
+    fetch(toysURL, {
+      method:'POST',
+     headers: { 
+         'Content-type': 'application/json',
+         'accept': 'application/json'
+     },
+     body: JSON.stringify({
+      name: toyName,
+      image: toyImage,
+      likes: 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(addDivToy)
+    event.target.reset()
+  })
+ }
+
+
+  
+
+
+
+
+
+
